46be3bcdaa6c9e3bc7972c2c9f55e118642d692a
SIddharth SIngh <siddharth.singh2019@vitstudent.ac.in>
2020-10-09 00:19:18 +0530

    models done

--- api/models/Post.go
+++ api/models/Post.go
@@ -59,3 +59,71 @@ func (p *Post) SavePost(db *gorm.DB) (*Post, error) {
 	}
 	return p, nil
 }
+
+//FindAllPost ...
+func (p *Post) FindAllPost(db *gorm.DB) (*[]Post, error) {
+	var err error
+	posts := []Post{}
+	err = db.Debug().Model(&Post{}).Limit(100).Find(&posts).Error
+	if err != nil {
+		return &[]Post{}, err
+	}
+	if len(posts) > 0 {
+		for i := range posts {
+			err = db.Debug().Model(&User{}).Where("id = ?", posts[i].AuthorID).Take(&posts[i].Author).Error
+			if err != nil {
+				return &[]Post{}, err
+			}
+		}
+	}
+	return &posts, nil
+}
+
+//FindPostByID ...
+func (p *Post) FindPostByID(db *gorm.DB, pid uint64) (*Post, error) {
+	var err error
+	err = db.Debug().Model(&Post{}).Where("id = ?", p.ID).Take(&p).Error
+	if err != nil {
+		return &Post{}, err
+	}
+	if p.ID != 0 {
+		err = db.Debug().Model(&User{}).Where("id = ?", p.AuthorID).Take(&p.Author).Error
+		if err != nil {
+			return &Post{}, err
+		}
+	}
+	return p, nil
+}
+
+//UpdateAPost ...
+func (p *Post) UpdateAPost(db *gorm.DB) (*Post, error) {
+	var err error
+	err = db.Debug().Model(&Post{}).Where("id = ?", p.ID).Updates(
+		Post{
+			Title:     p.Title,
+			Content:   p.Content,
+			UpdatedAt: time.Now(),
+		},
+	).Error
+	if p.ID != 0 {
+		err = db.Debug().Model(&User{}).Where("id = ?", p.AuthorID).Take(&p.Author).Error
+		if err != nil {
+			return &Post{}, err
+		}
+	}
+	return p, nil
+}
+
+//DeleteAPost ...
+func (p *Post) DeleteAPost(db *gorm.DB, pid uint64, uid uint32) (int64, error) {
+
+	db = db.Debug().Model(&Post{}).Where("id = ? and author_id = ?", pid, uid).Take(&Post{}).Delete(&Post{})
+
+	if db.Error != nil {
+		if gorm.IsRecordNotFoundError(db.Error) {
+			return 0, errors.New("Post not found")
+		}
+		return 0, db.Error
+	}
+	return db.RowsAffected, nil
+}
